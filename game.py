4c14bae1a2bedc7a4c9dd116a77acc7e36b59e13
Stephen Yount <stephenyount@mail.weber.edu>
2025-01-23 10:04:50 -0700

    Added input for players name with response to player after such.

--- game.py
+++ game.py
@@ -5,9 +5,15 @@ Version: 1.0
 Description:
 This is a text-based adventure game where the player makes choices
 to navigate through a mysterious forest.
-
 '''
+
 
 # Welcome message and introduction
 print("Welcome to the Adventure Game!")  
 print("Your journey begins here...")
+
+# Ask for the player's name
+player_name = input ("what is your name, adventurere?")
+
+# Concatenate strings to create a personalized message
+print("Wlecome " + player_name + ", Your journey begins now!")
