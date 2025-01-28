af7e49f6c1c2b559a496e4aae31f10581d04bfb1
Stephen Yount <stephenyount@mail.weber.edu>
2025-01-28 10:11:55 -0700

    Added forest and first decision with if options that might not be working

--- game.py
+++ game.py
@@ -11,10 +11,35 @@ to navigate through a mysterious forest.
 
 # Welcome message and introduction
 print("Welcome to the Adventure Game!")  
-print("Your journey begins here...")
+print('Your journey begins here...')
 
 # Ask for the player's name
-player_name = input ("what is your name, adventurere?")
+player_name = input ("what is your name, adventurere? ")
 
 # Concatenate strings to create a personalized message
 print("Wlecome " + player_name + ", Your journey begins now!")
+
+# Use an f-string to display the same message in a more readable way
+# print(f"Welcome {player_name}, Your journey begins now!")
+
+#Describe the starting area
+starting_area ="""
+You find yourself in a dark foest.
+The sound of rustling leaves fulls the air.
+A faint path lies ahead, leading deeper into the
+unknown...
+"""
+
+print(starting_area)
+
+#Ask the player for their first decision
+decision = input("Do you wish to take the path? (yes or no): ".lower)()
+
+#Respond to player based on choice
+if decision == "yes":
+    print(f"Brave choice{player_name}! You step onto the path and venture forward!")
+
+if decision == "no":
+    print(player_name+ ", you decide to wait. Perhaps courage will find you later.") # Concatenation example")
+else:
+    print("Confused, you stand still, unsure of what to do.")
