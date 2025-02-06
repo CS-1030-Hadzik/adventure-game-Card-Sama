f5335686f543d979c39bb7396c58786ddf4ae78b
Stephen Yount <stephenyount@mail.weber.edu>
2025-02-06 10:01:16 -0700

    (REF) Added loop to invalid input of starting area

--- game.py
+++ game.py
@@ -8,7 +8,6 @@ This is a text-based adventure game where the player makes choices
 to navigate through a mysterious forest.
 '''
 
-
 # Welcome message and introduction
 print("Welcome to the Adventure Game!")  
 print('Your journey begins here...')
@@ -22,8 +21,9 @@ print("Wlecome " + player_name + ", Your journey begins now!")
 # Use an f-string to display the same message in a more readable way
 # print(f"Welcome {player_name}, Your journey begins now!")
 
+
 #Describe the starting area
-starting_area ="""
+starting_area = """
 You find yourself in a dark forest.
 The sound of rustling leaves fills the air.
 A faint path lies ahead, leading deeper into the
@@ -35,17 +35,24 @@ print(starting_area)
 #Ask the player for their first decision
 decision = input("Do you wish to take the path? (yes or no): ").lower()
 
-#Respond to player based on choice
+
+#Invalid input loop
+while decision not in ["yes", "no"]:
+    print("Confused you stand still, unsure what to do you lay down to rest your head thinking about if you should say 'yes' or 'no'.")
+    print("You wake up after a power nap ready to make a decision... What will you choose?")
+    decision = input("Do you wish to take the path? (yes or no): ").lower()
+
+
+#Respond to player based on choice, if invalid choice, loop back to decision
 if decision == "yes":
     print(f"Brave choice{player_name}! You step onto the path and venture forward!")
 
-#No decision section, goal is to have this section print and cylce back to option or force close game, which eveer one fits better.
 
+#No decision section, goal is to have this section print and cylce back to option or force close game, which eveer one fits better.
 elif decision == "no":
     print(player_name+ ", you decide to wait. Perhaps courage will find you later.") # Concatenation example")
     print("Hours pass and the sun sets. You lay down under a tree and wake up the next day faced with a decision...")
 
 
-#Not valid answer section
-else: 
-    print("Confused, you stand still, unsure of what to do.")
+
+
